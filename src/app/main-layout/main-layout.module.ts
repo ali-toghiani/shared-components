df638380e545c94f999a479254dc69959a2fd3f4
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-05-27 17:56:30 +0430

    feat(main-layout): create header

--- src/app/main-layout/main-layout.module.ts
+++ src/app/main-layout/main-layout.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainLayoutComponent } from './main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    MainLayoutComponent
+    MainLayoutComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
