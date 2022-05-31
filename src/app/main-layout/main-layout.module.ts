a885c48c0a92227526302831a5e68e1e2c6a950a
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-05-31 18:28:32 +0430

    feat(main-layout.module): declare ActionbarComp and import routerModule and svgIconModule

--- src/app/main-layout/main-layout.module.ts
+++ src/app/main-layout/main-layout.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainLayoutComponent } from './main-layout.component';
 import { HeaderComponent } from './components/header/header.component';
-
-
+import { ActionbarComponent } from './components/actionbar/actionbar.component';
+import { RouterModule } from "@angular/router";
+import { AngularSvgIconModule} from "angular-svg-icon";
 
 @NgModule({
   declarations: [
     MainLayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    ActionbarComponent,
+
+  ],
+  exports: [
+    MainLayoutComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    AngularSvgIconModule
   ]
 })
 export class MainLayoutModule { }
