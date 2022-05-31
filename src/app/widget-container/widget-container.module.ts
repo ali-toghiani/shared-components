ecce7fe2b973d9cd578041854db1eefcad31bc9d
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-05-31 18:50:34 +0430

    feat(widget-container): create widget

---
+++ src/app/widget-container/widget-container.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WidgetContainerComponent } from './widget-container.component';
+
+
+
+@NgModule({
+  declarations: [
+    WidgetContainerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class WidgetContainerModule { }
