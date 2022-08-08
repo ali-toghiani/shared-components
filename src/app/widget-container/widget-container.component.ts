c8d5d6e5494cb5d6ab2ffbc5015170c96a4d9bfe
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-08 17:57:01 +0430

    feat(widget-container.comp): add isLoading property and implement corresponding template

--- src/app/widget-container/widget-container.component.ts
+++ src/app/widget-container/widget-container.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class WidgetContainerComponent implements OnInit {
   @Input() title: string;
+  @Input() isLoading = false;
   constructor() { }
 
   ngOnInit(): void {
