21ffdbbaf535889c3552bc0f7dc9da83a07e9199
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-08 17:50:43 +0430

    refactor/feat(widget-container.module): import NzSpin, NzIcon

--- src/app/widget-container/widget-container.module.ts
+++ src/app/widget-container/widget-container.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
 import { WidgetContainerComponent } from './widget-container.component';
-
-
 
 @NgModule({
     declarations: [
-        WidgetContainerComponent
+      WidgetContainerComponent
     ],
     exports: [
-        WidgetContainerComponent
+      WidgetContainerComponent
     ],
     imports: [
-        CommonModule
+      CommonModule,
+      NzSpinModule,
+      NzIconModule
     ]
 })
 export class WidgetContainerModule { }
