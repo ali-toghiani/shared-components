855ab754f75bd72ccbef04dd5ffcf55bd9d1c7bf
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-27 11:42:51 +0430

    feat(view-chart.module): declare & export ChartTabsComponent, import: TranslateModule + Nz[Table,Tabs,Icon,Button]module

--- src/app/view-chart/view-chart.module.ts
+++ src/app/view-chart/view-chart.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TranslateModule } from "@ngx-translate/core";
+
+import { NzTableModule } from "ng-zorro-antd/table";
+import { NzTabsModule } from "ng-zorro-antd/tabs";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { NzButtonModule } from "ng-zorro-antd/button";
+
 import { ViewChartComponent} from "@shared-components/src/app/view-chart/view-chart/view-chart.component";
+
+import { ChartTabsComponent } from './chart-tabs/chart-tabs.component';
 
 @NgModule({
   declarations: [
-    ViewChartComponent
+    ViewChartComponent,
+    ChartTabsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NzTableModule,
+    NzTabsModule,
+    TranslateModule,
+    NzIconModule,
+    NzButtonModule
   ],
   exports: [
-    ViewChartComponent
+    ViewChartComponent,
+    ChartTabsComponent
   ]
 })
 export class ViewChartModule { }
