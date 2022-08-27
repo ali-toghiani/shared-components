fedfb3c5b4ace819c3e47e289b84d83a101c9fff
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-27 11:33:51 +0430

    refactor(header.component): remove input property, subscribe to MainLayoutService, add logout button

--- src/app/main-layout/components/header/header.component.ts
+++ src/app/main-layout/components/header/header.component.ts
@@ -1,18 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {HeaderModel} from "../../models/header.model";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Subscription } from "rxjs";
+
+import { HeaderModel } from "../../models/header.model";
+import { MainLayoutService, TokenService } from "@services";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy{
 
-  @Input() headerConfig: HeaderModel;
+  headerConfig: HeaderModel;
 
-  constructor() { }
+  subscriptions = new Subscription();
+
+  constructor(
+    private tokenService: TokenService,
+    private layoutService: MainLayoutService
+  ) { }
 
   ngOnInit(): void {
+    this.subscriptions.add(
+      this.layoutService.headerConfigSubject.subscribe(header => {
+        this.headerConfig = header;
+      })
+    )
   }
 
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+  onLogout(): void{
+    this.tokenService.onSignOut();
+  }
 }
